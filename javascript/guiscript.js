ca4b53d64cca230f2a27b0769626f86a715355d6
LorinStoni <lorin.steiner@bluewin.ch>
2024-05-26 18:58:23 +0200

    Contact form on button click, new CSS class .hover-effect.slide-in
    Show contact form on button click
    Create class .hover-effect.slide-in
    Shift CSS classes of icons, gallery and image-container (in the file styles.css)
    Improve some responsive sizes (like images in image containers (CSS class .image-container))

--- javascript/guiscript.js
+++ javascript/guiscript.js
@@ -1,8 +1,11 @@
+/*
+    responsible menu
+*/
 const navMenu = document.querySelector('.nav-menu-icon');
 const mobileNavigation = document.querySelector('.mobile-nav');
 
 function toggleNavigationVisibility() {
-    const expanded = navMenu.getAttribute('aria-expanded') === "true" || false;
+    const expanded = navMenu.getAttribute('aria-expanded') === "true";
     navMenu.setAttribute('aria-expanded', !expanded);
     mobileNavigation.setAttribute('aria-expanded', !expanded);
 }
@@ -10,4 +13,14 @@ function toggleNavigationVisibility() {
 const menuIcon = document.querySelector('.icon.menu');
 console.log(menuIcon);
 menuIcon.addEventListener('click', toggleNavigationVisibility);
+
+/*
+    contact form
+*/
+const buttonContactForm = document.getElementById('button-contact-form');
+const contactForm = document.getElementById('contact-form');
+buttonContactForm.addEventListener('click', function (e) {
+    var expanded = contactForm.getAttribute('aria-expanded') === "true";
+    contactForm.setAttribute('aria-expanded', !expanded);
+});
 
